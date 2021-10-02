50defc2e74b8335b9a3f2289a38f964315cd8225
santosh maurya <santosh.maurya@wakencode.com>
2021-10-02 21:34:41 +0530

    pug template added

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const server = app.listen(port, () => { console.log("server listening  at port number", port) })
+
+app.set("view engine", "pug");
+app.set("views", "views");
+app.get("/", (req, res, next) => {
+    res.status(200).render("home");
+
+})
