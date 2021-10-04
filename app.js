e94c37d3924bf80d032e1cb068757e0ff4cd6c6d
santosh maurya <santosh.maurya@wakencode.com>
2021-10-04 17:25:03 +0530

    login page created

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const middleWere = require("./middlewere");
+const path = require('path');
 const server = app.listen(port, () => { console.log("server listening  at port number", port) })
 
 app.set("view engine", "pug");
 app.set("views", "views");
-app.get("/", (req, res, next) => {
-    res.status(200).render("home");
+app.use(express.static(path.join(__dirname, "/public")));
+
+const loginRoutes = require("./routes/loginRoutes");
+app.use("/login", loginRoutes);
+const registerRoutes = require("./routes/registerRoutes");
+app.use("/register", registerRoutes);
+app.get("/", middleWere.requirLogin, (req, res, next) => {
+    var payload = {
+        pageTitle: "Welcome"
+    }
+    res.status(200).render("home", payload);
 
 })
---
+++ middlewere.js
@@ -0,0 +1,9 @@
+exports.requirLogin = (req, res, next) => {
+    if (req.session && req.session.user) {
+        return next();
+    }
+    else {
+        return res.redirect("./login");
+    }
+
+}
